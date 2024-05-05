13932793eafbe15f969d071f197f858e3333f343
Christophe Delord <cdelord.fr>
2024-05-05 19:10:27 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,127 @@
+obfuscate.lua
+=============
+
+`obfuscate.lua` hides Lua scripts in encrypted scripts.
+The encrypted script are still executable by any Lua or LuaX interpreter.
+
+> “Everyone is a moon, and has a dark side which he never shows to anybody.”
+> -― Mark Twain
+
+Disclaimer
+==========
+
+The goal of `obfuscate.lua` is actually not to hide or protect sources.
+Its algorithm is very poor and cryptographically weak.
+The main purpose is to avoid unintended modifications.
+
+If you need to protect your secrets, please **DO NOT USE** `obfuscate.lua`!
+
+Usage
+=====
+
+`obfuscate.lua` requires [LuaX](https://github.com/CDSoft/luax)
+but can produce plain Lua encrypted scripts.
+
+```
+Usage: obfuscate [-h] [-o output] [-x] [-k key] [-b] [-s] [-z] input
+
+Lua/LuaX script obfuscator
+
+Arguments:
+   input                 Input script
+
+Options:
+   -h, --help            Show this help message and exit.
+   -o output             Output file
+   -x                    Generate a LuaX script
+   -k key                Encryption key
+   -b                    Write Lua bytecode (-bb compiles the outer chunk)
+   -s                    Don't write debug information
+   -z                    Compress the script with lz4
+```
+
+Target
+======
+
+`obfuscate.lua` produces plain Lua scripts that can be executed by a Lua interpreter without any additional dependency.
+
+The `-x` option makes scripts for LuaX instead of Lua.
+In this case, it uses LuaX functions to decrypt the initial script.
+Otherwise the script can run with any plain Lua interpreter.
+
+Encryption
+==========
+
+The `-k` option gives the encryption key (a default key is provided by `obfuscate.lua`).
+
+Lua and LuaX use different encryption algorithms.
+
+Lua scripts are encrypted with a simple PRNG to "xor" script bytes.
+The code to decrypt is added to the output script.
+
+LuaX scripts are encrypted with [`crypt.rc4`](https://github.com/CDSoft/luax/blob/master/doc/crypt.md#rc4-encryption).
+These scripts can only be executed with [LuaX](https://github.com/CDSoft/luax).
+
+Bytecode
+========
+
+By default, `obfuscate.lua` stores the Lua script source in the encrypted script.
+
+The encrypted script has two levels of code.
+The first level is the input script.
+The second one is the decryption code.
+
+The option `-b` compiles the first level to Lua bytecode.
+It can be used twice (`-b -b` or `-bb`) to also encrypt the second level.
+
+The option `-s` strips the debug information from the bytecode.
+It makes the output smaller but error messages will carry less information.
+
+Compression
+===========
+
+LuaX scripts can be compressed with the `-z` option.
+Sources are compressed with [`lz4`](https://github.com/CDSoft/luax/blob/master/doc/lz4.md).
+
+Example
+=======
+
+To compile, strip, compress and encrypt `hello.lua`, targeting LuaX:
+
+``` sh
+$ obfuscate.lua -x -bb -s -z hello.lua -o hello-encrypted.lua
+```
+
+or shorter:
+
+``` sh
+$ obfuscate.lua -xbbsz hello.lua -o hello-encrypted.lua
+```
+
+`hello-encrypted.lua` can be executed by LuaX:
+
+``` sh
+$ hello-encrypted.lua       # note that the shebang line calls luax instead of lua
+$ luax hello-encrypted.lua  # ignore the shebang and explicitly run the script with luax
+```
+
+License
+=======
+
+    This file is part of obfuscate.lua.
+
+    obfuscate.lua is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    obfuscate.lua is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with obfuscate.lua.  If not, see <https://www.gnu.org/licenses/>.
+
+    For further information about obfuscate.lua you can visit
+    https://cdelord.fr/obfuscate.lua
---
+++ obfuscate.lua
@@ -0,0 +1,181 @@
+#!/usr/bin/env luax
+
+-- This file is part of obfuscate.lua.
+--
+-- obfuscate.lua is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+--
+-- obfuscate.lua is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with obfuscate.lua.  If not, see <https://www.gnu.org/licenses/>.
+--
+-- For further information about obfuscate.lua you can visit
+-- https://cdelord.fr/obfuscate.lua
+
+local F = require "F"
+local fs = require "fs"
+local crypt = require "crypt"
+local term = require "term"
+
+---------------------------------------------------------------------
+-- Command line
+---------------------------------------------------------------------
+
+local cli = (function()
+    local parser = require "argparse"()
+        : name "obfuscate"
+        : description "Lua/LuaX script obfuscator"
+    parser : argument "input"
+        : description "Input script"
+        : args "1"
+        : argname "input"
+        : target "input"
+    parser : option "-o"
+        : description "Output file"
+        : args "1"
+        : argname "output"
+        : target "output"
+    parser : flag "-x"
+        : description "Generate a LuaX script"
+    parser : option "-k"
+        : description "Encryption key"
+        : args "1"
+        : argname "key"
+        : target "key"
+    parser : flag "-b"
+        : description "Write Lua bytecode (-bb compiles the outer chunk)"
+        : count "0-2"
+        : target "bytecode"
+    parser : flag "-s"
+        : description "Don't write debug information"
+        : target "strip"
+    parser : flag "-z"
+        : description "Compress the script with lz4"
+        : target "compress"
+    return F{
+        key = "“Everyone is a moon, and has a dark side which he never shows to anybody.” ― Mark Twain",
+    } : patch(parser:parse(arg))
+end)()
+
+---------------------------------------------------------------------
+-- Read the input script
+---------------------------------------------------------------------
+
+local input = assert(fs.read(cli.input))
+
+---------------------------------------------------------------------
+-- Remove the shebang line
+---------------------------------------------------------------------
+
+input = input:gsub("^#!", "--")
+
+---------------------------------------------------------------------
+-- Compute the encryption key
+---------------------------------------------------------------------
+
+local function chunks_of(n, s)
+    local chunks = F{}
+    while #s > 0 do
+        chunks[#chunks+1], s = s:split_at(n)
+    end
+    return chunks
+end
+
+local key_size = F.floor(16 + (#input-16)*(256-16)/(4096-16))
+key_size = F.max(16, F.min(256, key_size))
+
+local key = chunks_of(key_size, input:rc4(cli.key)) : fold1(crypt.rc4)
+
+---------------------------------------------------------------------
+-- Compile the script to Lua bytecode
+---------------------------------------------------------------------
+
+if cli.bytecode >= 1 then
+    local compiled_chunk = assert(load(input, "@"..cli.input))
+    local bytecode = assert(string.dump(compiled_chunk, cli.strip))
+    input = bytecode
+end
+
+---------------------------------------------------------------------
+-- Produce a self decrypting script
+---------------------------------------------------------------------
+
+local byte = string.byte
+local char = string.char
+
+local function escape(s)
+    return s:gsub(".", function(c) return ("\\%d"):format(byte(c)) end)
+end
+
+local encrypted_script
+
+if cli.x then
+
+    local unlz4 = ""
+    if cli.compress then
+        local compressed_input = input:lz4()
+        if #compressed_input < #input-8 then
+            input = compressed_input
+            unlz4 = ":unlz4()"
+        end
+    end
+
+    encrypted_script = F.I { b=escape(input:rc4(key)), k=escape(key), unlz4=unlz4 } [===[
+return load(('$(b)'):unrc4'$(k)'$(unlz4))()
+]===]
+
+else
+
+    local a, c = 6364136223846793005, 1
+    local seed = tonumber(key:hash(), 16)
+    local r = seed
+    local xs = {}
+    for i = 1, #input do
+        local b = byte(input, i, i+1)
+        r = r*a + c
+        xs[i] = char(b ~ ((r>>33) & 0xff))
+    end
+
+    encrypted_script = F.I { a=a, c=c, b=escape(table.concat(xs)), seed=seed } [===[
+local b,a,c,r,x,bt,ch,l,tc='$(b)',$(a),$(c),$(("0x%x"):format(seed)),{},string.byte,string.char,load,table.concat;for i=1,#b do r=r*a+c x[i]=ch(bt(b,i)~((r>>33)&0xff))end;return l(tc(x))()
+]===]
+
+end
+
+if cli.bytecode >= 2 then
+    local compiled_chunk = assert(load(encrypted_script))
+    local bytecode = assert(string.dump(compiled_chunk, cli.strip))
+    encrypted_script = bytecode
+end
+
+---------------------------------------------------------------------
+-- Add the shebang line
+---------------------------------------------------------------------
+
+encrypted_script = F.str {
+    cli.x
+        and "#!/usr/bin/env luax\n"
+        or  "#!/usr/bin/env lua\n",
+    encrypted_script,
+}
+
+---------------------------------------------------------------------
+-- Save the encrypted file
+---------------------------------------------------------------------
+
+if cli.output then
+    assert(fs.write_bin(cli.output, encrypted_script))
+    fs.chmod(cli.output, fs.aX|fs.aR|fs.uW)
+
+elseif not term.isatty(io.stdout) then
+    io.stdout:write(encrypted_script)
+
+else
+    error "Can not write the encrypted script to stdout"
+end
